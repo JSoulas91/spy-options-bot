6ec29b3224917ca684132afa3d7cdcb4947cee7d
JSoulas91 <jordansoulas@gmail.com>
2025-06-05 13:05:00 -0500

    Update websocket_client.py
    File cant figure out how to delete

--- data/websocket_client.py
+++ data/websocket_client.py
@@ -1,12 +1,1 @@
 # data/websocket_client.py
-from alpaca_trade_api.stream import Stream
-from config import ALPACA_API_KEY, ALPACA_SECRET_KEY, ALPACA_BASE_URL
-
-def start_websocket(on_bar_callback):
-    stream = Stream(ALPACA_API_KEY, ALPACA_SECRET_KEY, base_url=ALPACA_BASE_URL)
-
-    @stream.on_bar("SPY")
-    async def handle_bar(bar):
-        await on_bar_callback(bar)
-
-    stream.run()
